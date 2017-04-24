4dc0a897f9d75df9e5a1cffa0e51523a73cd6cdb
jcuratcha <curatchaj@gmail.com>
2017-04-24 18:49:40 -0500

    Fixed problems with plane, fixed camera position

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ const ASPECT = WIDTH / HEIGHT;
 const NEAR = 0.1;
 const FAR = 10000
 
+const moveMagnitude = 3;
+
 // get DOM element to attach to
 const container = document.querySelector('#container');
+const scene = new THREE.Scene();
 
 // Create WebGL renderer & camera
 const renderer = new THREE.WebGLRenderer();
@@ -18,9 +21,13 @@ const camera = new THREE.PerspectiveCamera(
 		ASPECT,
 		NEAR,
 		FAR
-	)
+	);
 
-const scene = new THREE.Scene();
+camera.position.z = 450;
+camera.position.y = 150;
+
+// camera.rotation.x = Math.PI / 4;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 scene.add(camera);
 
@@ -40,9 +47,7 @@ var planeMaterial = new THREE.MeshPhongMaterial({
 
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-plane.position.z = -350;
-
-plane.rotation.x = 90;
+plane.rotation.x = Math.PI / 2;
 plane.rotation.y = 0;
 plane.rotation.z = 0;
 
@@ -56,11 +61,11 @@ scene.add(plane);
 //--------------------------------------------------
 
 // create the particle variables
-var particleCount = 8000,
-    particles = new THREE.Geometry(),
-    pMaterial = new THREE.MeshPhongMaterial({
-      color: 0xffffff,
-      size: 2,
+var particleCount = 16000;
+var particles = new THREE.Geometry();
+var pMaterial = new THREE.PointsMaterial({
+      color: 0xaaaaaa,
+      size: 2.5,
     });
 
 pMaterial.lights.value = true;
@@ -70,8 +75,8 @@ for (var p = 0; p < particleCount; p++) {
 
 	// create a particle with random
 	// position values, -250 -> 250
-	var pY = Math.random() * 100;
-	var radius = pY/3;
+	var pY = Math.random() * 125;
+	var radius = Math.random() * pY/3;
 	var angle = Math.random() * 360;
 
 	var pX = radius * Math.cos(angle);
@@ -96,11 +101,6 @@ for (var p = 0; p < particleCount; p++) {
 // create the particle system
 var particleSystem = new THREE.Points(particles, pMaterial);
 
-// particleSystem.position.y = -150;
-particleSystem.position.z = -350;
-
-// particleSystem.rotation.z = 180;
-
 // add it to the scene
 scene.add(particleSystem);
 
@@ -111,18 +111,52 @@ scene.add(particleSystem);
 // Main light
 const pointLight = new THREE.PointLight(0xffffff);
 
-pointLight.position.x = 10;
+pointLight.position.x = 0;
 pointLight.position.y = 50;
-pointLight.position.z = 130;
+pointLight.position.z = 10;
 
 scene.add(pointLight);
+
+//--------------------------------------------------
+// Keypresses
+//--------------------------------------------------
+
+function onKeyDown(evt) {
+  switch (evt.keyCode) {
+	case 37: // 'left arrow'
+		particleSystem.position.x = particleSystem.position.x - moveMagnitude; 
+		break;
+	case 38: // 'up arrow'
+		particleSystem.position.z = particleSystem.position.z - moveMagnitude; 
+		break;
+	case 39: // 'right arrow'
+		particleSystem.position.x = particleSystem.position.x + moveMagnitude; 
+		break;
+	case 40: // 'down arrow'
+		particleSystem.position.z = particleSystem.position.z + moveMagnitude; 
+		break;
+	case 33: // 'page up'
+		particleSystem.position.y = particleSystem.position.y + moveMagnitude; 
+		break;
+	case 34: // 'page down'
+		particleSystem.position.y = particleSystem.position.y - moveMagnitude; 
+		break;
+
+  }
+}
+
+window.addEventListener('keydown', onKeyDown, false );
 
 //--------------------------------------------------
 // Update Loop
 //--------------------------------------------------
 
 function update() {
-	particleSystem.rotation.y += 0.08;
+	particleSystem.rotation.y += 0.18;
+
+	var event = window.event ? window.event : null;
+	if (event !== null)
+		console.log(event.keyCode);
 
 	renderer.render(scene, camera);
 	requestAnimationFrame(update);	
