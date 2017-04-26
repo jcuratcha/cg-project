ccd198d95ea6f4253a4c97d7a24fb68182ba8188
jcuratcha <curatchaj@gmail.com>
2017-04-25 21:00:05 -0500

    Changed to cannon.js, organize code into functions

--- main.js
+++ main.js
@@ -1,165 +1,252 @@
 var gl; // WebGL context
 
-const WIDTH = 800;
-const HEIGHT = 600;
-
-const VIEW_ANGLE = 45;
-const ASPECT = WIDTH / HEIGHT;
-const NEAR = 0.1;
-const FAR = 10000
-
-const moveMagnitude = 3;
-
-// get DOM element to attach to
-const container = document.querySelector('#container');
-const scene = new THREE.Scene();
-
-// Create WebGL renderer & camera
-const renderer = new THREE.WebGLRenderer();
-const camera = new THREE.PerspectiveCamera(
-		VIEW_ANGLE,
-		ASPECT,
-		NEAR,
-		FAR
-	);
-
-camera.position.z = 450;
-camera.position.y = 150;
-
-// camera.rotation.x = Math.PI / 4;
-camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-scene.add(camera);
-
-renderer.setSize(WIDTH, HEIGHT);
-
-container.appendChild(renderer.domElement); 
-
 //--------------------------------------------------
-// Base Plane/Terrain
+// Globals/Constants
 //--------------------------------------------------
 
-var planeGeometry = new THREE.PlaneGeometry(1, 1);
-var planeMaterial = new THREE.MeshPhongMaterial({
-	color: 0x338933,
-	side: THREE.DoubleSide
-});
-
-var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-plane.rotation.x = Math.PI / 2;
-plane.rotation.y = 0;
-plane.rotation.z = 0;
-
-plane.scale.x = 500;
-plane.scale.y = 500;
-
-scene.add(plane);
+const MOVE_MAG = 3;
+const FIELD_X_LIMIT = 250;
+const FIELD_Y_LIMIT = 20;
+const FIELD_Z_LIMIT = 250;
 
 //--------------------------------------------------
-// Particle system
+// Instance Variables
 //--------------------------------------------------
 
-// create the particle variables
-var particleCount = 16000;
-var particles = new THREE.Geometry();
-var pMaterial = new THREE.PointsMaterial({
-      color: 0xaaaaaa,
-      size: 2.5,
-    });
+var container;
+var camera, scene, renderer;
+var particleSystem;
 
-pMaterial.lights.value = true;
+initCannon();
+initScene();
+animate();
 
-// now create the individual particles
-for (var p = 0; p < particleCount; p++) {
-
-	// create a particle with random
-	// position values, -250 -> 250
-	var pY = Math.random() * 125;
-	var radius = Math.random() * pY/3;
-	var angle = Math.random() * 360;
-
-	var pX = radius * Math.cos(angle);
-	var pZ = radius * Math.sin(angle);
-
-	//x and z position depend on y  	
-
-	// var pX = (Math.random() * 500-250) % pY/2;
-	// var pZ = (Math.random() * 500-250) % pY/2;
-	var particle = new THREE.Vector3(pX, pY, pZ);
-
-	// create a velocity vector
-	particle.velocity = new THREE.Vector3(
-	0,              // x
-	-Math.random(), // y: random vel
-	0);             // z
-
-  // add it to the geometry
-  particles.vertices.push(particle);
+function initCannon() {
+	console.log("Initializing physics");
 }
 
-// create the particle system
-var particleSystem = new THREE.Points(particles, pMaterial);
+function initScene() {
 
-// add it to the scene
-scene.add(particleSystem);
+	console.log("Initializing Scene");
+	//--------------------------------------------------
+	// Camera
+	//--------------------------------------------------
 
-//--------------------------------------------------
-// Lights
-//--------------------------------------------------
+	const VIEW_ANGLE = 45;
+	const ASPECT = window.innerWidth / window.innerHeight;
+	const NEAR = 0.1;
+	const FAR = 10000
 
-// Main light
-const pointLight = new THREE.PointLight(0xffffff);
+	// get DOM element to attach to
+	container = document.createElement('div');
+	document.body.appendChild(container);
 
-pointLight.position.x = 0;
-pointLight.position.y = 50;
-pointLight.position.z = 10;
+	scene = new THREE.Scene();
 
-scene.add(pointLight);
+	// Create WebGL renderer & camera
+	renderer = new THREE.WebGLRenderer();
+	camera = new THREE.PerspectiveCamera(
+			VIEW_ANGLE,
+			ASPECT,
+			NEAR,
+			FAR
+		);
 
-//--------------------------------------------------
-// Keypresses
-//--------------------------------------------------
+	camera.position.z = 450;
+	camera.position.y = 150;
+
+	// camera.rotation.x = Math.PI / 4;
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+	scene.add(camera);
+
+	scene.fog = new THREE.Fog( 0x000000, 500, 10000);
+
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	container.appendChild(renderer.domElement); 
+	var controls = new THREE.TrackballControls(camera, renderer.domElement);
+
+	controls.rotateSpeed = 1.0;
+	controls.zoomSpeed = 1.2;
+	controls.panSpeed = 0.8;
+	controls.noZoom = false;
+	controls.noPan = false;
+	controls.staticMoving = true;
+	controls.dynamicDampingFactor = 0.3;
+	controls.keys = [ 65, 83, 68 ];
+
+
+	//--------------------------------------------------
+	// Base Plane/Terrain
+	//--------------------------------------------------
+
+	var planeGeometry = new THREE.PlaneGeometry(1, 1);
+	var planeMaterial = new THREE.MeshPhongMaterial({
+		color: 0x338933,
+		side: THREE.DoubleSide
+	});
+
+	var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+	plane.rotation.x = Math.PI / 2;
+	plane.rotation.y = 0;
+	plane.rotation.z = 0;
+
+	plane.scale.x = 500;
+	plane.scale.y = 500;
+
+	scene.add(plane);
+
+	//--------------------------------------------------
+	// Particle system
+	//--------------------------------------------------
+
+	// create the particle variables
+	var particleCount = 8000;
+	var particles = new THREE.Geometry();
+	var pMaterial = new THREE.PointsMaterial({
+	      color: 0xaaaaaa,
+	      size: 2.5,
+	    });
+
+	pMaterial.lights.value = true;
+
+	// now create the individual particles
+	for (var p = 0; p < particleCount; p++) {
+
+		// create a particle with random
+		// position values, -250 -> 250
+		var pY = Math.random() * 125;
+		var radius = Math.random() * pY/3;
+		var angle = Math.random() * 360;
+
+		var pX = radius * Math.cos(angle);
+		var pZ = radius * Math.sin(angle);
+
+		var particle = new THREE.Vector3(pX, pY, pZ);
+
+		// create a velocity vector
+		particle.velocity = new THREE.Vector3(
+		0,              // x
+		-Math.random(), // y: random vel
+		0);             // z
+
+	  // add it to the geometry
+	  particles.vertices.push(particle);
+	}
+
+	// create the particle system
+	particleSystem = new THREE.Points(particles, pMaterial);
+
+	particleSystem.position.x = 0;
+	particleSystem.position.y = 0;
+	particleSystem.position.z = 0;
+
+	// add it to the scene
+	scene.add(particleSystem);
+
+	//--------------------------------------------------
+	// Scene-Physical Objects
+	//--------------------------------------------------
+
+	var boxGeometry1 = new THREE.BoxGeometry( 50, 20, 50 );
+	var boxMaterial1 = new THREE.MeshPhongMaterial({
+			color: 0x347298
+		});
+
+	var box1 = new THREE.Mesh(boxGeometry1, boxMaterial1);
+	box1.position.set(0, 10, 60);
+
+	scene.add(box1);
+
+	var boxGeometry2 = new THREE.CubeGeometry( 20, 10, 70 );
+	var boxMaterial2 = new THREE.MeshPhongMaterial({
+			color: 0x999988
+		});
+
+	var box2 = new THREE.Mesh(boxGeometry2, boxMaterial2);
+
+	box2.position.set(50, 5, 40);
+	scene.add(box2);
+
+	var boxGeometry3 = new THREE.CubeGeometry( 20, 50, 10 );
+	var boxMaterial3 = new THREE.MeshPhongMaterial({
+			color: 0xf2a348
+		});
+
+	var box3 = new THREE.Mesh(boxGeometry3, boxMaterial3);
+
+	box3.position.set(-30, 25, -100);
+	scene.add(box3);
+
+	//--------------------------------------------------
+	// Lights
+	//--------------------------------------------------
+
+	// Main light
+	const pointLight = new THREE.PointLight(0xffffff);
+
+	pointLight.position.x = 0;
+	pointLight.position.y = 50;
+	pointLight.position.z = 10;
+
+	scene.add(pointLight);
+
+	window.addEventListener('keydown', onKeyDown, false );
+
+}
 
 function onKeyDown(evt) {
-  switch (evt.keyCode) {
-	case 37: // 'left arrow'
-		particleSystem.position.x = particleSystem.position.x - moveMagnitude; 
-		break;
-	case 38: // 'up arrow'
-		particleSystem.position.z = particleSystem.position.z - moveMagnitude; 
-		break;
-	case 39: // 'right arrow'
-		particleSystem.position.x = particleSystem.position.x + moveMagnitude; 
-		break;
-	case 40: // 'down arrow'
-		particleSystem.position.z = particleSystem.position.z + moveMagnitude; 
-		break;
-	case 33: // 'page up'
-		particleSystem.position.y = particleSystem.position.y + moveMagnitude; 
-		break;
-	case 34: // 'page down'
-		particleSystem.position.y = particleSystem.position.y - moveMagnitude; 
-		break;
+	switch (evt.keyCode) {
 
-  }
+		case 68: // 'right'
+			particleSystem.position.x = particleSystem.position.x + MOVE_MAG; 
+			if (particleSystem.position.x > FIELD_X_LIMIT)
+				particleSystem.position.x = FIELD_X_LIMIT;
+			break;
+		case 65: // 'left'
+			particleSystem.position.x = particleSystem.position.x - MOVE_MAG; 
+			if (particleSystem.position.x < -FIELD_X_LIMIT)
+				particleSystem.position.x = -FIELD_X_LIMIT;
+			break;
+		case 83: // 'down'
+			particleSystem.position.z = particleSystem.position.z + MOVE_MAG;
+			if (particleSystem.position.z > FIELD_Z_LIMIT)
+				particleSystem.position.z = FIELD_Z_LIMIT; 
+			break;
+		case 87: // 'up'
+			particleSystem.position.z = particleSystem.position.z - MOVE_MAG;
+			if (particleSystem.position.z < -FIELD_Z_LIMIT)
+				particleSystem.position.z = -FIELD_Z_LIMIT;  
+			break;
+		case 81: // 'raise'
+			particleSystem.position.y = particleSystem.position.y + MOVE_MAG;
+			if (particleSystem.position.y > FIELD_Y_LIMIT)
+				particleSystem.position.y = FIELD_Y_LIMIT;
+			break;
+		case 69: // 'lower'
+			particleSystem.position.y = particleSystem.position.y - MOVE_MAG; 
+			if (particleSystem.position.y < -FIELD_Y_LIMIT)
+				particleSystem.position.y = -FIELD_Y_LIMIT;
+			break;
+
+	}
 }
 
-window.addEventListener('keydown', onKeyDown, false );
+function animate() {
+	requestAnimationFrame(animate);	
 
-//--------------------------------------------------
-// Update Loop
-//--------------------------------------------------
-
-function update() {
 	particleSystem.rotation.y += 0.18;
 
 	var event = window.event ? window.event : null;
 	if (event !== null)
 		console.log(event.keyCode);
 
-	renderer.render(scene, camera);
-	requestAnimationFrame(update);	
+	render();
 }
 
-requestAnimationFrame(update);
+function render() {
+	renderer.render(scene, camera);
+}
